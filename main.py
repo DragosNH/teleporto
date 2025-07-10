8e962b226f7fa0ab49ff8ca427b6c4d44bbe383b
Dragoş <edragos33@gmail.com>
2025-07-10 16:06:47 +0200

    added the code in the right place

--- main.py
+++ main.py
@@ -1,50 +1,9 @@
 import pygame
-from titlescreen import UIElement
+from titlescreen import UIElement, title_screen
 from gameState import GameState
 from firstLevel import first_level
 
-def title_screen(screen):
-    start_btn = UIElement(
-        center_position=(540, 300),
-        font_size=30,
-        bg_rgb=(27, 36, 72),
-        text_rgb=(228, 219, 183),
-        text="Start Game",
-        action=GameState.NEWGAME,
-    )
-    quit_btn = UIElement(
-        center_position=(540, 400),
-        font_size=30,
-        bg_rgb=(27, 36, 72),
-        text_rgb=(228, 219, 183),
-        text="Quit",
-        action=GameState.QUIT,
-    )
 
-    buttons = [start_btn, quit_btn]
-
-    while True:
-        mouse_up = False
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                return GameState.QUIT
-            if e.type == pygame.MOUSEBUTTONUP and e.button == 1:
-                mouse_up = True
-            if e.type == pygame.KEYDOWN:
-                if e.key == pygame.K_ESCAPE:
-                    return GameState.QUIT
-            if e.type == start_btn:
-                return GameState.NEWGAME
-
-        screen.fill((27, 36, 72))
-
-        for button in buttons:
-            ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
-            if ui_action is not None:
-                return ui_action
-            button.draw(screen)
-
-        pygame.display.flip()
 
 
 def main():
--- titlescreen.py
+++ titlescreen.py
@@ -2,7 +2,7 @@ import pygame
 import pygame.freetype
 from pygame.sprite import Sprite
 from pygame.rect import Rect
-from enum import Enum
+from gameState import GameState
 
 # Title Screen buttons
 def create_surface(text, font_size, text_rgb, bg_rgb):
@@ -40,3 +40,46 @@ class UIElement(Sprite):
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
+
+def title_screen(screen):
+    start_btn = UIElement(
+        center_position=(540, 300),
+        font_size=30,
+        bg_rgb=(27, 36, 72),
+        text_rgb=(228, 219, 183),
+        text="Start Game",
+        action=GameState.NEWGAME,
+    )
+    quit_btn = UIElement(
+        center_position=(540, 400),
+        font_size=30,
+        bg_rgb=(27, 36, 72),
+        text_rgb=(228, 219, 183),
+        text="Quit",
+        action=GameState.QUIT,
+    )
+
+    buttons = [start_btn, quit_btn]
+
+    while True:
+        mouse_up = False
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                return GameState.QUIT
+            if e.type == pygame.MOUSEBUTTONUP and e.button == 1:
+                mouse_up = True
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_ESCAPE:
+                    return GameState.QUIT
+            if e.type == start_btn:
+                return GameState.NEWGAME
+
+        screen.fill((27, 36, 72))
+
+        for button in buttons:
+            ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+            if ui_action is not None:
+                return ui_action
+            button.draw(screen)
+
+        pygame.display.flip()
